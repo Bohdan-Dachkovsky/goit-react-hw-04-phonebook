0a00f1f3912ce197d1bc622082b8e69ec0e24234
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-12-02 18:45:40 +0200

    Change getvisiblecontacts to another func

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import { useState, useEffect } from 'react'
 import ContactList from './components/ContactList/ContactList'
 import Filter from './components/Filter/Filter'
 import ContactForm from './components/ContactForm/ContactForm'
-const initalContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]
+// const initalContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ]
 const App = () => {
-  const [contacts, setContacts] = useState(initalContacts)
+  const [contacts, setContacts] = useState([])
   const [filter, setFilter] = useState('')
 
   const addContact = (phone) => {
-    const searchSameName = contacts.map((cont) => {
-      return cont.name === phone.name
-    })
+    const searchSameName = contacts.name === phone.name
+
+    if (phone.name.length === 0) {
+      alert('Fields must be filled!')
+    }
     if (searchSameName) {
       alert(`${phone.name} is already in contacts`)
     } else if (phone.name.length === 0) {
@@ -27,21 +29,15 @@ const App = () => {
         id: uuidv4(),
       }
 
-      setContacts((contacts) => ({
-        contacts: [...contacts, contact],
-      }))
+      setContacts((prevState) => ({ ...prevState, ...contact }))
       console.log(` new persons added`)
-    }
-
-    if (phone.name.length === 0) {
-      alert('Fields must be filled!')
     }
   }
   useEffect(() => {
     const contacts = localStorage.getItem('contacts')
     const parsedContacts = JSON.parse(contacts)
     if (parsedContacts) {
-      setContacts({ contacts: parsedContacts })
+      setContacts({ parsedContacts })
     }
   }, [])
 
