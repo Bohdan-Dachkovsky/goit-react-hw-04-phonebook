59a1028a1ab005f254eaf9e53791c1f1e8a9bf1b
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-02-17 01:27:43 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import { useState, useEffect } from 'react'
 import ContactList from './components/ContactList/ContactList'
 import Filter from './components/Filter/Filter'
 import ContactForm from './components/ContactForm/ContactForm'
-// const initalContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ]
+
 const App = () => {
   const [contacts, setContacts] = useState([])
   const [filter, setFilter] = useState('')
